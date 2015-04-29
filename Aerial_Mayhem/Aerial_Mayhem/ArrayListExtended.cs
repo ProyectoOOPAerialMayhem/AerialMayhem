ec3cdb2a37a162f7700fec41a8434cc0e50034d8
thefang12 <thefang12@gmail.com>
2015-04-29 16:31:17 -0500

    Added AnimatedSprite and BasicSprite classes

---
+++ Aerial_Mayhem/Aerial_Mayhem/ArrayListExtended.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using System.Collections;
+
+namespace Aerial_Mayhem
+{
+    class ArrayListExtended : ArrayList
+    {
+        public void Replace(int index, Object obj)
+        {
+            this[index] = obj;
+        }
+    }
+}
+
