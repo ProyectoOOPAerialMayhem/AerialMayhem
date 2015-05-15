551df1d450f0435bd52fe2e6bb21ebe702bab3aa
thefang12 <thefang12@gmail.com>
2015-05-15 02:25:04 -0500

    Buttons!!

--- Aerial_Mayhem/Aerial_Mayhem/DrawUtils/Button.cs
+++ Aerial_Mayhem/Aerial_Mayhem/DrawUtils/Button.cs
@@ -8,16 +8,23 @@ namespace Aerial_Mayhem.DrawUtils
 {
     enum ButtonStates 
     { 
-
+        normal,rollover,mouseDown,active
     }
     public class Button :IDrawable
     {
         SimpleSprite[] states;
         Rectangle pos;
         ButtonStates state;
-        public Button(Rectangle pos,SimpleSprite mouseDown)
+        /// <summary>
+        /// Standar  position rectangle
+        /// states[0]=normal,states[1]=rollover,state[2]=mouseDown,state[4]Active
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="states"></param>
+        public Button(Rectangle pos,SimpleSprite [] states)
         {
-
+            this.states = states;
+            state = ButtonStates.normal;
         }
         public void Update(GameTime gameTime)
         {
