76bbe20f00fefa2b0b9dd9b605bddc9062f5a691
mexicantacos <a01273613@itesm.mx>
2015-05-15 10:13:51 -0500

    kjniuihiRevert "ya final"
    This reverts commit e11c29becacf815dacb1fa5f25c6ff6c48aaf44a.
    
    Conflicts:
    	Aerial_Mayhem/Aerial_Mayhem/Game1.cs

--- Aerial_Mayhem/Aerial_Mayhem/GameScenes.cs
+++ Aerial_Mayhem/Aerial_Mayhem/GameScenes.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Aerial_Mayhem
 
 {
-    enum GameScenes
+     enum GameScenes
     {
         StartScreen, OptionScreen, CharacterSlector, LevelSelector, Pause, GameOver,Level1,Level2,Level3,LeaderBoard
     }
